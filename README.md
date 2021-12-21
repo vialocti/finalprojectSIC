ccb6225c0f2e084ad0fa4a3150e08c9e563f6750
Alfredo Brizuela <alfredo@debian>
2021-12-21 18:38:04 -0300

    primer commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # coding-project-template
+# last_token: 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
--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest=principal * rate * years / 100;
+    var year = new Date().getFullYear() + years;
+
+    document.getElementById("result").innerHTML="Si deposito dolares recibio <mark>" + interest +"</mark> <br/> year";
     
 }
         
