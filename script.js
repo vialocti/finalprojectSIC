a972f10e99e65b76e74dbbd234eaf51c34c26df9
Bambino Brizuela <bambino@debian.localbrizuela.com.ar>
2021-12-26 20:46:12 -0300

    final one

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-function compute()
-{
+function calcular(){
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    if (principal>0) {
+    var rate=document.getElementById("rate").value;
+    var years =  document.getElementById("years").value; 
+    var year=new Date().getFullYear() + parseInt(years);
+    var interest = principal * years * rate /100;
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"% </mark>\<br\>You will receive an amount of <mark>" + interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 
-    var interest=principal * rate * years / 100;
-    var year = new Date().getFullYear() + years;
-
-    document.getElementById("result").innerHTML="Si deposito dolares recibio <mark>" + interest +"</mark> <br/> year";
+    }
+    else{
+        alert("the entered value must be positive");
+        document.getElementById("principal").focus();
+    }
     
 }
 
-function updateRate(){
-    var rate= document.getElementById("rate").value;
-    document.getElementById("rate_value").innerText=rate;
+function loadpage(){
+    document.getElementById("rate").value="10.25";
 }
 
-function loadpage(){
-    document.getElementById("rate").value="10.5"
+function updateRange(){
+
+    var ratevalue=document.getElementById("rate").value;
+    document.getElementById("rate_value").innerText=ratevalue;
+
 }
-        
